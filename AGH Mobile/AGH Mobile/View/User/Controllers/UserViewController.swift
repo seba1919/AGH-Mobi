d744ec5525b316f7f1426ac5122b2eb9e20662cb
Mateusz Bąk <bakmateusz@icloud.com>
2019-04-17 08:51:16 +0200

    Add temporary button for segue to 'About As'

--- AGH Mobile/AGH Mobile/View/User/Controllers/UserViewController.swift
+++ AGH Mobile/AGH Mobile/View/User/Controllers/UserViewController.swift
@@ -21,11 +21,37 @@ class UserViewController: UIViewController {
     
     var userView: UserView { return self.view as! UserView }
     
+    private let tempbutton = TemporaryButton(title: "Go to about as!")
+        .addTarget(self, action: #selector(nextButtonActionHandler(_:)), for: .touchUpInside)
+        .build()
+    
     // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
     // MARK: - Lifecycle
     
     override func loadView() {
         self.view = UserView(frame: UIScreen.main.bounds)
+        setupUI()
+    }
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Action handlers
+    
+    @objc private func nextButtonActionHandler(_ sender: UIButton) {
+        navigationController?.pushViewController(AboutAsViewController(), animated: true)
+    }
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Setup
+    
+    func setupUI() {
+        view.addSubview(tempbutton)
+
+        NSLayoutConstraint.activate([
+            tempbutton.bottomAnchor.constraint(equalTo: userView.safeAreaLayoutGuide.bottomAnchor, constant: -36),
+            tempbutton.widthAnchor.constraint(equalToConstant: 200),
+            tempbutton.heightAnchor.constraint(equalToConstant: 50),
+            tempbutton.centerXAnchor.constraint(equalTo: userView.safeAreaLayoutGuide.centerXAnchor),
+            ])
     }
     
 }
