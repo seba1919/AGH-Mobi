756da8aeac838c113e16534a31d640d126b5b8f0
Mateusz Bąk <bakmateusz@icloud.com>
2019-05-15 07:39:42 +0200

    Add Building project structure

--- AGH Mobile/AGH Mobile/View/Building/Controllers/BuildingViewController.swift
+++ AGH Mobile/AGH Mobile/View/Building/Controllers/BuildingViewController.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
 //
 
-import Foundation
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Import
+
+import UIKit
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Implementation
+
+class BuildingViewController: UIViewController {
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Properties
+    
+    
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Lifecycle
+    
+    
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Setup
+    
+    private func setupNavigationAttributs() {
+
+    }
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Actions
+    
+    private func setupActions() {
+        
+    }
+    
+}
--- AGH Mobile/AGH Mobile/View/Building/Views/BuildingView.swift
+++ AGH Mobile/AGH Mobile/View/Building/Views/BuildingView.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
 //
 
-import Foundation
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Import
+
+import UIKit
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Implementation
+
+class BuildingView: UIView {
+    
+    
+    
+    
+}
