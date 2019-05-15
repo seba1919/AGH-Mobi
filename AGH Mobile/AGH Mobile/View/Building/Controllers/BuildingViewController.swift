abb02cc5e2e62b602ee70c433bfce33850e34616
Mateusz Bąk <bakmateusz@icloud.com>
2019-05-15 07:37:02 +0200

    Init

---
+++ AGH Mobile/AGH Mobile/View/Building/Controllers/BuildingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BuildingViewController.swift
+//  AGH Mobile
+//
+//  Created by Mateusz Bąk on 15/05/2019.
+//  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
+//
+
+import Foundation
---
+++ AGH Mobile/AGH Mobile/View/Building/Views/BuildingView.swift
@@ -0,0 +1,9 @@
+//
+//  BuildingView.swift
+//  AGH Mobile
+//
+//  Created by Mateusz Bąk on 15/05/2019.
+//  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
+//
+
+import Foundation
