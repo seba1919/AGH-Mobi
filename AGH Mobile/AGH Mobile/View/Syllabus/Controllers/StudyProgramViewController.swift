3947e0d84bcafe7da59c1c5bb2046766647aaa00
Bartosz Kunat <mrclapslock@gmail.com>
2019-05-19 11:58:01 +0200

    Add controllers

---
+++ AGH Mobile/AGH Mobile/View/Syllabus/Controllers/StudyProgramViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StudyProgramViewController.swift
+//  AGH Mobile
+//
+//  Created by Bartosz Kunat on 19/05/2019.
+//  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
+//
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Import
+
+import UIKit
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Implementation
+
+class StudyProgramViewController: UIViewController {
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Instance Variables
+    
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
