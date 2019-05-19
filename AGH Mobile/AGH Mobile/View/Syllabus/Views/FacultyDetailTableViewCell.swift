4a59158a888f302b83580517eb6098b36a2595ad
Bartosz Kunat <mrclapslock@gmail.com>
2019-05-19 19:10:02 +0200

    Add FacultyDetailTableViewCell

--- AGH Mobile/AGH Mobile/View/Syllabus/Views/FacultyDetailTableViewCell.swift
+++ AGH Mobile/AGH Mobile/View/Syllabus/Views/FacultyDetailTableViewCell.swift
@@ -6,19 +6,97 @@
 //  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
 //
 
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Imports
+
 import UIKit
 
-class FacultyDetailTableViewCell: UITableView {
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Implementation
 
+class FacultyDetailTableViewCell: UITableViewCell {
+    
+    private struct Constants {
+        static let horizontalPadding: CGFloat = 48
+        static let horizontalSpacing: CGFloat = 10
+        static let verticalPadding: CGFloat = 16
+        static let verticalSpacing: CGFloat = 2
+        
+        // Font sizes
+        static let titleFontSize: CGFloat = 18
+    }
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Static Variables
+    
     static let reuseIdentifier: String = {
         return String.init(describing: self)
     }()
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - UI components
+    
+    var titleLbl: UILabel!
+    private var arrowImageView: UIImageView!
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Instance porperties
+    
+    var viewModel: FacultiesTableViewCellDummyViewModel! {
+        didSet {
+            titleLbl.text = viewModel.title
+            titleLbl.sizeToFit()
+        }
     }
-    */
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUpView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - View setup methods
+    
+    private func setUpView() {
+        
+        // Right arrow image view
+        arrowImageView = UIImageView(image: #imageLiteral(resourceName: "arrow_right"))
+        arrowImageView.contentMode = .scaleAspectFit
+        addSubview(arrowImageView)
+        arrowImageView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(Constants.verticalPadding)
+            make.bottom.equalToSuperview().offset(-Constants.verticalPadding)
+            make.right.equalToSuperview().offset(-Constants.horizontalPadding / 2)
+            make.width.equalTo(arrowImageView.frame.width)
+        }
+        
+        // Title label
+        titleLbl = UILabel()
+        titleLbl.textColor = .customGrayText
+        titleLbl.numberOfLines = 0
+        titleLbl.font = UIFont.systemFont(ofSize: Constants.titleFontSize, weight: .regular)
+        addSubview(titleLbl)
+        titleLbl.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(Constants.horizontalPadding)
+            make.right.lessThanOrEqualTo(arrowImageView.snp.left).offset(-Constants.horizontalPadding)
+            make.top.equalToSuperview().offset(Constants.verticalPadding)
+            make.bottom.equalToSuperview().offset(-Constants.verticalPadding)
+        }
+    }
+}
 
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Delete this when view model for Syllabus is available
+
+
+struct FacultyDetailTableViewCellDummyViewModel {
+    let title: String
 }
