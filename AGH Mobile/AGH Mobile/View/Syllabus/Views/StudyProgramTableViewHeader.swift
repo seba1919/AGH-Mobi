cfd34a3d06a18207550a4b000a8734e221cedf98
Bartosz Kunat <mrclapslock@gmail.com>
2019-05-23 13:18:16 +0200

    Added header

--- AGH Mobile/AGH Mobile/View/Syllabus/Views/StudyProgramTableViewHeader.swift
+++ AGH Mobile/AGH Mobile/View/Syllabus/Views/StudyProgramTableViewHeader.swift
@@ -9,13 +9,72 @@
 import UIKit
 
 class StudyProgramTableViewHeader: UITableViewHeaderFooterView {
-
+    
+    private struct Constants {
+        static let horizontalPadding: CGFloat = 36
+        static let horizontalSpacing: CGFloat = 10
+        static let verticalPadding: CGFloat = 24
+        static let verticalSpacing: CGFloat = 40
+        
+        // Font sizes
+        static let titleFontSize: CGFloat = 30
+        static let subtitleFontSize: CGFloat = 14
+    }
+    
     // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-    // MARK: - Static Variables
+    // MARK: - Static properties
     
     static let reuseIdentifier: String = {
         return String.init(describing: self)
     }()
     
-
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Instance properties
+    
+    
+    var sectionTitle: String? {
+        didSet {
+            subtitleLbl.text = sectionTitle
+            subtitleLbl.sizeToFit()
+        }
+    }
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - UI components
+    
+    var subtitleLbl: UILabel!
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Init
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - View setup
+    
+    private func setUpView() {
+        
+        // Subtitle label
+        subtitleLbl = UILabel()
+        subtitleLbl.textColor = .customGrayText
+        subtitleLbl.font = UIFont.systemFont(ofSize: Constants.subtitleFontSize, weight: .regular)
+        subtitleLbl.numberOfLines = 0
+        addSubview(subtitleLbl)
+        
+        // Only the first section has a titleLbl
+        subtitleLbl.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(Constants.verticalSpacing)//.priority(.medium)
+            make.bottom.equalToSuperview().offset(-Constants.verticalSpacing)
+            make.right.equalToSuperview().offset(-Constants.horizontalPadding)
+            make.left.equalToSuperview().offset(Constants.horizontalPadding)
+            
+        }
+    }
 }
