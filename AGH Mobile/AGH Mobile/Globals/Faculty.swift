40eaea284cc9f1512903ceb08d75e317d045c3c8
Sebastian Wiatrzyk <sebastian.wiatrzyk@capgemini.com>
2019-07-02 20:02:06 +0200

    Add enum for faculties

---
+++ AGH Mobile/AGH Mobile/Globals/Faculty.swift
@@ -0,0 +1,13 @@
+//
+//  Faculty.swift
+//  AGH Mobile
+//
+//  Created by Sebastian Wiatrzyk on 02/07/2019.
+//  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
+//
+
+import Foundation
+
+enum Faculty {
+    case EAIIB, IET, FIS, GG, H, IMIP, IMR, GGOS, IMC, O, MN, WNG, Z, EP, MS
+}
