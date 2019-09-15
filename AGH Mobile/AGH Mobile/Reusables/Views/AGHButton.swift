336562731be0edd99f9fc5fa001d5dedff2a2958
Mateusz Bąk <mateusz.bak@x-kom.pl>
2019-09-15 19:30:35 +0200

    Marge agh button from AM-021

--- AGH Mobile/AGH Mobile/Reusables/Views/AGHButton.swift
+++ AGH Mobile/AGH Mobile/Reusables/Views/AGHButton.swift
@@ -1,59 +1,135 @@
+//
+//  AGHButton.swift
+//  AGH Mobile
+//
+//  Created by Mateusz Bąk on 03/05/2019.
 //  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
+//
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Import
 
 import UIKit
 import SnapKit
 
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// MARK: - Implementation
+
 final class AGHButton {
  
-    // MARK: - Private properties
-    private let AGHButton: UIButton
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Properties
     
+    // Button
+    private let AGHButton: UIButton
+    // Styles
+    public enum Style {
+        case normal
+        case withLeftAccessory
+    }
+    // Components of view
+    private lazy var title: UILabel = {
+        let lebel = UILabel()
+        lebel.translatesAutoresizingMaskIntoConstraints = false
+        lebel.textColor = .white
+        lebel.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        lebel.textAlignment = .center
+        return lebel
+    }()
+    
+    private lazy var leftAccesory: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
     // MARK: - Init
-    init(title: String) {
+    
+    init(title: String, style: Style = .normal, leftAccesoryName: String? = nil) {
         AGHButton = UIButton(type: .system)
-        AGHButton.setTitle(title, for: .normal)
-        self.setupLayout()
+        self.title.text = title
+        if (leftAccesoryName != nil) {
+            leftAccesory.image = UIImage(named: leftAccesoryName ?? "star")
+        }
+        
+        switch style {
+        case .normal: setupNormalStyle()
+        case .withLeftAccessory: setupWithLeftAccessoryStyle()
+        }
     }
     
-    // MARK: - Setups
-    private func setupLayout() {
-        self.setupConstraintsAndCornerRadius()
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MARK: - Setup
+    
+    // Common Properties
+    private func setupCommonProperties() {
+        setupHeightConstraintsAndCornerRadius()
         AGHButton.translatesAutoresizingMaskIntoConstraints = false
         AGHButton.backgroundColor = .mainRed
-        AGHButton.setTitleColor(.white, for: .normal)
-        AGHButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .medium)
-        AGHButton.titleLabel?.textAlignment = .center
     }
     
-    private func setupConstraintsAndCornerRadius() {
-        var height: CGFloat = 26
+    // Styles
+    private func setupNormalStyle() {
+        setupCommonProperties()
+        AGHButton.addSubview(title)
+        setupNormalConstraints()
+    }
+    
+    private func setupWithLeftAccessoryStyle() {
+        setupCommonProperties()
+        AGHButton.addSubview(title)
+        AGHButton.addSubview(leftAccesory)
+        setupWithLeftAccessoryConstraints()
+    }
+    
+    // Constraints
+    private func setupNormalConstraints() {
+        title.snp.makeConstraints { (make) in
+            make.centerX.equalTo(AGHButton.snp.centerX)
+            make.centerY.equalTo(AGHButton.snp.centerY)
+        }
+    }
+    
+    private func setupWithLeftAccessoryConstraints() {
+        let leftAccesoryHeight: CGFloat = 17
+        let freeSpace: CGFloat = 5
+        title.snp.makeConstraints { (make) in
+            make.centerY.equalTo(AGHButton)
+            make.centerX.equalTo(AGHButton).offset(leftAccesoryHeight/2 + freeSpace/2)
+        }
+        leftAccesory.snp.makeConstraints { (make) in
+            make.right.equalTo(title.snp.left).offset(-freeSpace)
+            make.centerY.equalTo(AGHButton)
+            make.size.equalTo(CGSize(width: leftAccesoryHeight, height: leftAccesoryHeight))
+        }
+    }
+    
+    private func setupHeightConstraintsAndCornerRadius() {
+        let screenHeight = UIScreen.main.bounds.height
+        var halfOfHeight: CGFloat = 26
         
-        if UIScreen.isSmallSize() {
-            height = 17
-            AGHButton.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .medium)
-        } else if UIScreen.isMediumSize() {
-            height = 22
-        } else if UIScreen.isLargeSize() {
-            height = 24
-        } else if UIScreen.isBigSize() {
-            height = 26
+        if (screenHeight < 569) {
+            halfOfHeight = 17
+            title.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        } else if (screenHeight > 569 && screenHeight < 668) {
+            halfOfHeight = 22
+        } else if (screenHeight > 668 && screenHeight < 737) {
+            halfOfHeight = 24
+        } else if screenHeight > 737 {
+            halfOfHeight = 26
         }
+        AGHButton.layer.cornerRadius = halfOfHeight
         AGHButton.snp.makeConstraints { (make) in
-            make.height.equalTo(height * 2)
+            make.height.equalTo(halfOfHeight * 2)
         }
-        AGHButton.layer.cornerRadius = height
     }
-}
-
-extension AGHButton {
     
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
     // MARK: - Public Methods
-    public func addTarget(_ target: Any?, action: Selector, for event: UIControl.Event) -> Self {
-        AGHButton.addTarget(target, action: action, for: event)
-        return self
-    }
     
     public func build() -> UIButton {
         return AGHButton
     }
+    
 }
