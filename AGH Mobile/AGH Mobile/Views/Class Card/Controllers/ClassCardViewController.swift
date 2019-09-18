0725c45792fbbc3d21f45ed4e90021f332203911
WIktorMaselek <wiktor.maselek@gmail.com>
2019-09-18 11:58:44 +0200

    Initial commit

---
+++ AGH Mobile/AGH Mobile/Views/Class Card/Controllers/ClassCardViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ClassCardViewController.swift
+//  AGH Mobile
+//
+//  Created by Macbook on 18/09/2019.
+//  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
+//
+
+import Foundation
