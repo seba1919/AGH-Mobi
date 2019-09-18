49e24877be783dcaa521cf2884f25056ec3ffa9b
WIktorMaselek <wiktor.maselek@gmail.com>
2019-09-18 13:52:04 +0200

    Add subject type label and marks

--- AGH Mobile/AGH Mobile/Views/Class Card/Views/ClassCardView.swift
+++ AGH Mobile/AGH Mobile/Views/Class Card/Views/ClassCardView.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
 //
 
+// MARK: - Import
+
 import UIKit
+
+// MARK: - Implementation
 
 class ClassCardView: UIView {
     
@@ -15,11 +19,13 @@ class ClassCardView: UIView {
     private lazy var screenHeight = self.frame.height
     private lazy var screenWidth = self.frame.width
     private lazy var spacing = screenHeight * 0.03
+    private lazy var spacingInRows = screenHeight * 0.0125
     private let leftMargin: CGFloat = 20
     private let rightMargin: CGFloat = -20
 
     
     // MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -28,6 +34,8 @@ class ClassCardView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Setup view methods
     
     public func setupUI() {
         setupViews()
@@ -38,10 +46,13 @@ class ClassCardView: UIView {
         self.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
         [subjectName,
-         separator].forEach({addSubview($0)})
+         separator,
+         subjectType].forEach({addSubview($0)})
         
     }
     
+    // MARK: - Components of View
+
     private lazy var subjectName: UILabel = {
         let lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
@@ -56,22 +67,39 @@ class ClassCardView: UIView {
         let v = SeparatorView().build()
         return v
     }()
-
-
     
+    private lazy var subjectType: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        lbl.textColor = .mainRed
+        lbl.textAlignment = .left
+        lbl.text = "Wykład"
+        return lbl
+    }()
+
+
+    // MARK: - Constraints
     
     private func setupConstraints() {
         
         subjectName.snp.makeConstraints { (make) in
             make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(spacing)
             make.left.equalToSuperview().offset(leftMargin)
-            make.rightMargin.equalToSuperview().offset(rightMargin)
+            make.right.equalToSuperview().offset(rightMargin)
 
         }
         
         separator.snp.makeConstraints { (make) in
             make.top.equalTo(subjectName.snp.bottom).offset(spacing)
-            make.width.equalToSuperview()
+            make.left.equalToSuperview().offset(leftMargin)
+            make.right.equalToSuperview().offset(rightMargin)
+        }
+        
+        subjectType.snp.makeConstraints { (make) in
+            make.top.equalTo(separator.snp.bottom).offset(spacingInRows)
+            make.left.equalToSuperview().offset(leftMargin)
+            make.right.equalToSuperview().offset(rightMargin)
         }
 
 
