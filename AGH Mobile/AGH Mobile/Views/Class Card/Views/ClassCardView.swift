90d672a62998fb6be56e235b2646f7e03ed7f123
WIktorMaselek <wiktor.maselek@gmail.com>
2019-09-18 13:35:25 +0200

    Add subject label with separator

--- AGH Mobile/AGH Mobile/Views/Class Card/Views/ClassCardView.swift
+++ AGH Mobile/AGH Mobile/Views/Class Card/Views/ClassCardView.swift
@@ -12,6 +12,13 @@ class ClassCardView: UIView {
     
     // MARK: - Private properties
     
+    private lazy var screenHeight = self.frame.height
+    private lazy var screenWidth = self.frame.width
+    private lazy var spacing = screenHeight * 0.03
+    private let leftMargin: CGFloat = 20
+    private let rightMargin: CGFloat = -20
+
+    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,12 +37,44 @@ class ClassCardView: UIView {
     private func setupViews() {
         self.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
+        [subjectName,
+         separator].forEach({addSubview($0)})
+        
     }
+    
+    private lazy var subjectName: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.systemFont(ofSize: 35, weight: .bold)
+        lbl.textColor = .black
+        lbl.textAlignment = .left
+        lbl.text = "Bazy Danych"
+        return lbl
+    }()
+    
+    private lazy var separator: UIView = {
+        let v = SeparatorView().build()
+        return v
+    }()
+
+
     
     
     private func setupConstraints() {
         
+        subjectName.snp.makeConstraints { (make) in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(spacing)
+            make.left.equalToSuperview().offset(leftMargin)
+            make.rightMargin.equalToSuperview().offset(rightMargin)
+
+        }
         
+        separator.snp.makeConstraints { (make) in
+            make.top.equalTo(subjectName.snp.bottom).offset(spacing)
+            make.width.equalToSuperview()
+        }
+
+
     }
     
 
