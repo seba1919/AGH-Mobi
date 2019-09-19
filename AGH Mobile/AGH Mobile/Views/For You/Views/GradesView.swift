66c6f30515a1fc4d96142675843318ae960174b6
Mateusz Wagner <// TODO: - Method For Gettings Number Of buttonswagner.mateusz@zamel.pl>
2019-09-19 15:43:14 +0200

    Add base View components to GradesView and link them with Autolayout

--- AGH Mobile/AGH Mobile/Views/For You/Views/GradesView.swift
+++ AGH Mobile/AGH Mobile/Views/For You/Views/GradesView.swift
@@ -9,7 +9,8 @@ class GradesView: UIView {
     
     private lazy var screenHeight = self.frame.height
     private lazy var screenWidth = self.frame.width
-    
+    private lazy var topPadding = self.frame.height * 0.0225
+
     
     private lazy var lastGradeContainer: UIView = {
         let view = UIView()
@@ -17,10 +18,28 @@ class GradesView: UIView {
         return view
     }()
     
+    private lazy var termButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .brown
+        return button
+    }()
+    
     private lazy var segmentedControl: UISegmentedControl = {
         let control = UISegmentedControl()
         control.backgroundColor = .brown
         return control
+    }()
+    
+    private lazy var gradesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .brown
+        return tableView
+    }()
+    
+    private lazy var overallView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .brown
+        return view
     }()
 
     // MARK: - Init
@@ -45,7 +64,10 @@ extension GradesView {
     private func setupView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         addSubview(lastGradeContainer)
+        addSubview(termButton)
         addSubview(segmentedControl)
+        addSubview(gradesTableView)
+        addSubview(overallView)
     }
     
     // MARK: - Setup Constraints
@@ -58,11 +80,33 @@ extension GradesView {
             make.height.equalTo(Double(screenHeight) * SettingsView.userProfileHightMultipliValue)
         }
         
+        termButton.snp.makeConstraints { (make) in
+            make.top.equalTo(lastGradeContainer.snp.bottom).offset(topPadding)
+            make.left.equalTo(safeAreaLayoutGuide.snp.left).offset(20)
+            make.width.equalTo(Double(screenWidth) * 0.5)
+            make.height.equalTo(Double(screenHeight) * SettingsView.userProfileHightMultipliValue)
+        }
+        
         segmentedControl.snp.makeConstraints { (make) in
-            make.top.equalTo(lastGradeContainer.snp.bottom).offset(20)
+            make.top.equalTo(termButton.snp.bottom).offset(topPadding)
             make.centerX.equalTo(safeAreaLayoutGuide.snp.centerX)
             make.width.equalTo(Double(screenHeight) * SettingsView.userProfileHightMultipliValue * 2)
             make.height.equalTo(Double(screenHeight) * SettingsView.userProfileHightMultipliValue)
         }
+        
+        overallView.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(10)
+            make.right.equalToSuperview().offset(-10)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
+            make.height.equalTo(Double(screenHeight) * SettingsView.userProfileHightMultipliValue)
+        }
+
+        gradesTableView.snp.makeConstraints { (make) in
+            make.top.equalTo(segmentedControl.snp.bottom).offset(topPadding)
+            make.left.equalToSuperview().offset(10)
+            make.right.equalToSuperview().offset(-10)
+            make.bottom.equalTo(overallView.snp.top).offset(-10)
+        }
+        
     }
 }
