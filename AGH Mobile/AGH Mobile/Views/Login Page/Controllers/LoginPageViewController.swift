7849bda9f324e6c0830621309da9618e97bfac11
Sebastian Wiatrzyk <sebastian.wiatrzyk@capgemini.com>
2019-10-30 15:08:07 +0100

    Add missing code update for VC's

--- AGH Mobile/AGH Mobile/Views/Login Page/Controllers/LoginPageViewController.swift
+++ AGH Mobile/AGH Mobile/Views/Login Page/Controllers/LoginPageViewController.swift
@@ -49,7 +49,25 @@ extension LoginPageViewController {
         }
         
         loginPageView.pushSettingsVC = {
-            self.navigationController?.pushViewController(SettingsViewController(), animated: true)
+            guard let userWDLogin = self.loginPageView.idTextField.text else { return}
+            guard let userWDPassword = self.loginPageView.passwordTextField.text else { return}
+            if userWDLogin.isEmpty || userWDPassword.isEmpty {
+                self.loginPageView.loginButton.shake()
+                CustomNotifications.setupAlertOnMissingLoginCredentials()
+                return
+            }
+            self.loginPageView.loginButton.isUserInteractionEnabled = false
+            WDRouterNetworking()
+                .performWDLoginAction(userLogin: userWDLogin,
+                                      userPassword: userWDPassword) { isLoggedIn in
+                if isLoggedIn == .success {
+                    CustomNotifications.setupAlertOnLoginSuccess()
+                    self.navigationController?.pushViewController(SettingsViewController(), animated: true)
+                } else if isLoggedIn == .credentialsFailiture {
+                    CustomNotifications.setupAlertOnLoginFailiture()
+                }
+                self.loginPageView.loginButton.isUserInteractionEnabled = true
+            }
         }
         
         loginPageView.openRemindPasswordWeb = {
