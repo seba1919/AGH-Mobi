f76ba413dd91f5eb04bf20d004b3ea43a8757392
Mateusz Bąk <mateusz.bak@x-kom.pl>
2019-11-03 08:27:51 +0100

    Add setSelected for annotation view

--- AGH Mobile/AGH Mobile/Views/Maps/Views/MapAnnotationView.swift
+++ AGH Mobile/AGH Mobile/Views/Maps/Views/MapAnnotationView.swift
@@ -9,21 +9,43 @@ class MapAnnotationView: MKAnnotationView {
     
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
-        setupView()
+        setNotSelectedView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            setSelectedView()
+        } else {
+            setNotSelectedView()
+        }
+    }
 }
 
 extension MapAnnotationView {
     
-    private func setupView() {
+    private func setNotSelectedView() {
         if let annotation = annotation as? MapAnnotation {
             guard let customImage = UIImage(named: annotation.feature.properties.imageName) else {
                 return
             }
+            centerOffset = .zero
+            image = customImage
+        }
+    }
+    
+    private func setSelectedView() {
+        if let annotation = annotation as? MapAnnotation {
+            guard let customImage = UIImage(named: "\(annotation.feature.properties.imageName)_focused") else {
+                return
+            }
+            let offset = CGPoint(x: 0.0, y: -(customImage.size.height / 2.5) )
+            centerOffset = offset
             image = customImage
         }
     }
