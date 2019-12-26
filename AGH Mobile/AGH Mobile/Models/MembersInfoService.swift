4f9f334f08d492077e8e7307598bba7af5933b12
Sebastian Wiatrzyk <sebastian.wiatrzyk@capgemini.com>
2019-12-26 16:15:22 +0100

    Refactor to clean MVVM-C

---
+++ AGH Mobile/AGH Mobile/Models/MembersInfoService.swift
@@ -0,0 +1,9 @@
+//
+//  MembersInfoService.swift
+//  AGH Mobile
+//
+//  Created by Sebastian Wiatrzyk on 26/12/2019.
+//  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
+//
+
+import Foundation
---
+++ AGH Mobile/AGH Mobile/Models/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  AGH Mobile
+//
+//  Created by Sebastian Wiatrzyk on 26/12/2019.
+//  Copyright © 2019 AGH University of Science and Technology. All rights reserved.
+//
+
+import Foundation
